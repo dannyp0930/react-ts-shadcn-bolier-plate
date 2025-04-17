83928d7ee260b1d37b46dd9edeb1c49ba08861c1
dannyp0930 <dannyp0930@onoffmix.com>
2025-04-17 17:16:43 +0900

    feat: home link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ export default function Layout() {
   return (
     <div>
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/event">이벤트 관리</Link>
         <Link to="/user">사용자 관리</Link>
       </nav>
