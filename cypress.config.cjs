95377fccb5cf0439b0a65a9d44717e954c0fec32
dannyp0930 <dannyp0930@onoffmix.com>
2025-04-18 12:00:03 +0900

    feat: cypress 환경 설정

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,5 +1,22 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+const dotenv = require('dotenv')
+
+const env = process.env.CYPRESS_ENV || 'local'
+let envFile = '.env.development.local'
+
+if (env === 'dev') {
+  envFile = '.env.development'
+} else if (env === 'prod') {
+  envFile = '.env.production'
+}
+
+dotenv.config({ path: envFile })
+
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: process.env.VITE_BASE_URL,
+    env: {
+      API_URL: process.env.VITE_API_URL,
+    },
   },
-};
+})
